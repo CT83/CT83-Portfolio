3ec95a4ea61ca19a0c54bd27b2cfb055316e1e59
Rohan Sawant <rohansawantct83@gmail.com>
2020-04-04 17:48:14 +0530

    Added new testimonial

--- src/containers/testimonials/Testimonials.js
+++ src/containers/testimonials/Testimonials.js
@@ -11,7 +11,7 @@ class Testimonials extends Component {
       repo: [{
         name: "Sajjad Rostami",
         subtitle: "AI PhD Student, Human & Robot Interaction Developer",
-        url:"https://www.linkedin.com/in/sajjad-rostami-438515a6/",
+        url: "https://www.linkedin.com/in/sajjad-rostami-438515a6/",
         text: `Rohan has an extremely strong grasp over the intricacies, complexities, and challenges of working with Deep Learning, Machine Learning, and Software in general. 
         I reached out to Rohan when I needed help with an OpenCV Python issue I was having trouble with. He was surprisingly excited to help me with it, Rohan was able to fix something in less than an hour. He even went on to answer my corresponding StackOverflow. So that others could benefit from it as well. 
         Rohan is technically strong, a real people person and someone whom I can see myself collaborating with for several projects to come in the near future. `
@@ -19,7 +19,7 @@ class Testimonials extends Component {
       {
         name: "Arelene Andrews",
         subtitle: "Context Tester, Writer",
-        url:"https://www.linkedin.com/in/arlene-andrews-a02a7a107/",
+        url: "https://www.linkedin.com/in/arlene-andrews-a02a7a107/",
         text: `To Whom It May Concern:
         
 I have had the delight and pleasure to be acquainted with Rohan Sawant over the past few years, and watch his development skills, personality, and abilities grow and become more mature. He has gone from an instinctively curious person into one who can use that skill to explore new ideas, question their practicality, and create experiments and projects to show their value to the world.
@@ -32,6 +32,12 @@ I look forward to the continuing progress for Rohan's work – this is one to wa
 Yours,
 Arlene Andrews
         `
+      },
+      {
+        name: "Chaitanya Lakshmi Indukuri",
+        subtitle: "AWS Certified Solutions Architect - Associate | MTech - Cloud Computing",
+        url: "https://www.linkedin.com/in/chaitanya-lakshmi-indukuri-385688a4/",
+        text: `Not many people can be so efficient and helpful as Rohan was. He is knowledgeable and a pleasure to work with. When I was going through the hardships of my current project, he was somebody that I trusted and looked toward for guidance and advice.His overall presence had a positive impact on my work along with my individual knowledge. He helped me in understanding Azure infrastructure and many more. I'm sure he'll be an asset to any organization he joins.`
       },
       ]
     }
